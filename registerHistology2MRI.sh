96a6f7e0190735cb95508b1744371849482ef027
VANDA Lab <91102057+VANDAlab@users.noreply.github.com>
2024-11-07 15:06:42 -0500

    Add files via upload

--- registerHistology2MRI.sh
+++ registerHistology2MRI.sh
@@ -4,7 +4,8 @@ for i in $(ls /data/dadmah/frieve/Organized_files/*/Histology_Files/*.mnc);do
     path_t2=$(ls /data/dadmah/frieve/Organized_files/${id}/MRI_7T_blocks/*T2_TurboRARE*.mnc); #find the T2 TurboRARE image
 
     stain_1=$(echo ${i}|cut -d _ -f 10); # find the stain name
-    stain_2=$(echo ${i}|cut -d _ -f 11); # find the stain number
+    stain_2_tmp=$(echo ${i}|cut -d _ -f 11); # find the stain number
+    stain_2=$(echo ${stain_2_tmp}|cut -d . -f 1); # find the stain number
     path_xfm=$(ls /data/dadmah/frieve/Organized_files/${id}/Registration/*${stain_1}_${stain_2}*.xfm); #find the co-registration transformation (stain image to MRI)
 
     mincresample ${i} -like ${path_t2}  -transform ${path_xfm} /data/dadmah/frieve/Organized_files/${id}/Histology_Files/${id}_${stain_1}_${stain_2}_Histology_to_MRI.mnc -inv -clobber #transform the histology photo to MRI space 
